02eb0e3d7539ce5858ee687c98a8b6ac17110fca
Oren Shklarsky <oshklarsky@dwavesys.com>
2018-03-16 18:08:26 -0700

    use __import__ directly

--- sdk/dwave/samplers.py
+++ sdk/dwave/samplers.py
@@ -1,6 +1,5 @@
 import pkg_resources
-import importlib
 
 for entry_point in pkg_resources.iter_entry_points('dwave.samplers'):
-    module = importlib.import_module('{}'.format(entry_point.module_name))
+    module = __import__('{}'.format(entry_point.module_name))
     locals()[entry_point.name] = getattr(module, entry_point.name)

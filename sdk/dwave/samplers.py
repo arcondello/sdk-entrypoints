3240f4b7e71351a92c09c40ca2309fef75f6c7ea
Oren Shklarsky <oshklarsky@dwavesys.com>
2018-03-16 18:23:21 -0700

    use __all__

--- sdk/dwave/samplers.py
+++ sdk/dwave/samplers.py
@@ -1,6 +1,10 @@
 import pkg_resources
 import importlib
 
+# prevent entrypoints from appearing in the top-level namespace.
+# If we want fine grained control, we can add __all__.append(entry_point.ane) to the for loop below.
+__all__ = []
+
 for entry_point in pkg_resources.iter_entry_points('dwave.samplers'):
     module = importlib.import_module('{}'.format(entry_point.module_name))
     locals()[entry_point.name] = getattr(module, entry_point.name)
